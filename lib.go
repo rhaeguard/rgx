a10ab4e99a6b147930e649a960396f0b7f6d8624
Mansur <mensurowary@gmail.com>
2023-09-02 23:33:19 -0600

    remove unused code

--- lib.go
+++ lib.go
@@ -1,16 +1,11 @@
 package rgx
 
-import "fmt"
-
 func Check(regexString string, inputString string) bool {
 	memory := context{
 		pos:    0,
 		tokens: []regexToken{},
 	}
 	regex(regexString, &memory)
-	for _, t := range memory.tokens {
-		fmt.Printf("%+v\n", t)
-	}
 	nfaEntry := toNfa(&memory)
 
 	return nfaEntry.check(inputString, -1)
